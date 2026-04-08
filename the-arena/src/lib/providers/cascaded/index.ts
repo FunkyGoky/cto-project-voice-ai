1372f218e1a40ddb2eaccf5b84b151cbf7872719
Goki Kazama <goki6162@gmail.com>
2026-04-08 15:26:24 +0100

    Delete the-arena directory
    Deleting this as this will be uploaded in a separate repo

--- the-arena/src/lib/providers/cascaded/index.ts
+++
@@ -1,7 +0,0 @@
-// Cascaded pipeline barrel exports
-export { DeepgramSTTProvider } from "./deepgram-stt";
-export { ElevenLabsScribeProvider } from "./elevenlabs-stt";
-export { DeepgramTTSProvider } from "./deepgram-tts";
-export { ElevenLabsTTSProvider } from "./elevenlabs-tts";
-export { ProxiedLLMProvider } from "./llm-provider";
-export { CascadedPipelineOrchestrator } from "./pipeline";
